7f6033356f633968d8e846853b87eba069951cd8
FelixLamarche <felixlamarche28@gmail.com>
2025-04-19 14:36:44 -0400

    Faster testing of solutions

--- Devoir3_INF6102_H25/solver_advanced.py
+++ Devoir3_INF6102_H25/solver_advanced.py
@@ -1,6 +1,7 @@
 from utils import Instance, Solution
 
 import copy
+import itertools as it
 import numpy as np
 import random
 import time
@@ -34,10 +35,68 @@ class SolverAdvanced :
 
         self.timeout_time = timeout_time
 
-    def or_k_opt(self, k):
+    def first_or_k_opt_move(self, k):
         """
         OR-k-opt (relocate k elements)
-        Does not work for k>1
+        """
+        best_move = None
+        best_move_cost = 1e10
+
+        days = [i for i in range(self.J)]
+        random.shuffle(days)
+
+        for i in days:
+            if i+k > self.J:
+                continue
+
+            subseq = self.seq[i:i+k]
+            if k <= 3:
+                subseq_orders = [list(subseq_order) for subseq_order in it.permutations(subseq)]
+            else:
+                subseq_orders = [subseq]
+
+            for j in days:
+                if j >= i and j < i + k or j + k > self.J:
+                    continue
+
+                # Temporarily relocate product i -> j
+                insert_pos = j
+                if j > i:
+                    insert_pos -= k
+                new_seq = self.seq[:i] + self.seq[i+k:] # Remove subsequence
+
+                for subseq_order in subseq_orders:
+                    new_seq = new_seq[:insert_pos] + subseq_order + new_seq[insert_pos:] # Insert
+
+                    if not self.test_seq_stocks(new_seq) :
+                        continue
+
+                    new_cost = self.test_compute_cost(new_seq)
+                    if new_cost >= self.cost :
+                        continue
+
+                    if new_cost < best_move_cost:
+                        best_move = (i, j, subseq_order)
+                        best_move_cost = new_cost
+                if best_move:
+                    break
+            if best_move:
+                break
+
+        if best_move:
+            i, j, subseq = best_move
+            insert_pos = j
+            if j > i:
+                insert_pos -= k
+            self.seq = self.seq[:i] + self.seq[i+k:]
+            self.seq = self.seq[:insert_pos] + subseq + self.seq[insert_pos:]
+            self.cost = best_move_cost
+            self.compute_stocks()
+            print(f"Best Moved products {subseq} from day {i} to day {j}, k={k}")
+
+    def best_or_k_opt_move(self, k):
+        """
+        OR-k-opt (relocate k elements)
         """
         best_move = None
         best_move_cost = 1e10
@@ -47,9 +106,6 @@ class SolverAdvanced :
                 continue
 
             subseq = self.seq[i:i+k]
-
-            if all(p == -1 for p in subseq) :
-                continue
 
             for j in range(self.J - k + 1):
                 if j >= i  and j < i + k :
@@ -83,9 +139,9 @@ class SolverAdvanced :
             self.seq = self.seq[:insert_pos] + subseq + self.seq[insert_pos:]
             self.cost = best_move_cost
             self.compute_stocks()
-            print(f"Moved products {subseq} from day {i} to day {j}, k={k}")
+            print(f"Best Moved products {subseq} from day {i} to day {j}, k={k}")
 
-    def or_opt_move_1_pop_insert(self):
+    def first_or_opt_move_1_pop_insert(self):
         """
         OR-1-opt (relocate one element)
         Pops the product and inserts it at its new position pushing the products to the right
@@ -93,18 +149,17 @@ class SolverAdvanced :
         best_move = None
         best_move_cost = 1e10
 
-        for i in range(self.J):
-            # We will move the product i through all days by swapping it with the product of the next day
-            new_seq = self.seq[:]
-            # We set it first, then swap it with the next day's product for every day
-            product = new_seq.pop(i)
-            new_seq.insert(0, product)
-            for j in range(self.J):
-                # Swap product with the next day's product
-                if j > 0:
-                    temp = new_seq[j - 1]
-                    new_seq[j - 1] = new_seq[j]
-                    new_seq[j] = temp
+        days = [i for i in range(self.J)]
+        random.shuffle(days)
+
+        for i in days:
+            for j in days:
+                if i == j:
+                    continue
+
+                new_seq = self.seq[:]
+                product = new_seq.pop(i)
+                new_seq.insert(j, product)
                 
                 if not self.test_seq_stocks(new_seq) :
                     continue
@@ -125,7 +180,6 @@ class SolverAdvanced :
             self.cost = best_move_cost
             self.compute_stocks()
             print(f"Moved product {product} from day {i} to day {j}, pop-insert")
-
 
     def best_or_opt_move_1_pop_insert(self):
         """
@@ -165,7 +219,7 @@ class SolverAdvanced :
             self.compute_stocks()
             print(f"Best Moved product {product} from day {i} to day {j}, pop-insert")
 
-    def first_random_or_opt_move_1_insert_pop(self):
+    def first_or_opt_move_1_insert_pop(self):
         """
         OR-1-opt (relocate one element)
         Places a -1 where the product was and inserts it at its new position trying to push the products to the left 
@@ -275,7 +329,7 @@ class SolverAdvanced :
             self.compute_stocks()
             print(f"Best Moved product {product} from day {i} to day {j}, insert-pop")
 
-    def first_2_opt_random(self):
+    def first_2_opt_swap(self):
         """
         Best 2-opt swap local search
         """
@@ -312,7 +366,7 @@ class SolverAdvanced :
             self.compute_stocks()   # Updates stocks
             print(f"Swapped Day:{j1} and Day:{j2} for products:{product1} and {product2}")
 
-    def best_2_opt(self):
+    def best_2_opt_swap(self):
         """
         Best 2-opt swap local search
         """
@@ -348,22 +402,22 @@ class SolverAdvanced :
         """
         Does a variable neighborhood descent
         Reduced variable ngeighborhood search (takes random instead of best)
-        Results: 6.34/8 and is not fast
+        Results: 6.89/8 and is not fast
         """
         # Keep doing local search until no improvement is found
         neighborhoods_without_improvement = 0
-        NB_NEIGHBORHOODS = 25
+        NB_NEIGHBORHOODS = 8
         while neighborhoods_without_improvement < NB_NEIGHBORHOODS and time.time() < self.timeout_time:
             prev_cost = self.cost
 
             # Search a neighborhood
             if neighborhoods_without_improvement == 0:
-                self.first_2_opt_random()
+                self.first_or_opt_move_1_pop_insert()
             elif neighborhoods_without_improvement == 1:
-                self.first_random_or_opt_move_1_insert_pop()
+                self.first_or_opt_move_1_insert_pop()
             else:
-                k = neighborhoods_without_improvement - 1
-                self.or_k_opt(k)
+                k = neighborhoods_without_improvement
+                self.first_or_k_opt_move(k)
 
             # Check if we improved the solution
             # If we have not improved the solution, we go to the next neighborhood
@@ -408,7 +462,7 @@ class SolverAdvanced :
         prev_cost = -1
         while prev_cost != self.cost and time.time() < self.timeout_time:
             prev_cost = self.cost
-            self.or_k_opt(k)
+            self.first_or_k_opt_move(k)
 
             if self.cost < self.best_cost:
                 self.best_cost = self.cost
@@ -653,11 +707,10 @@ class SolverAdvanced :
             if c_prod > -1:
                 stock[c_prod] += 1
 
-            for c in range(self.C):
-                if c in self.day_demands[j]:
-                    stock[c] -= 1
-                    if stock[c] < 0:
-                        return False
+            for c in self.day_demands[j]:
+                stock[c] -= 1
+                if stock[c] < 0:
+                    return False
             
         return True
 
@@ -719,14 +772,13 @@ class SolverAdvanced :
     def test_compute_cost(self, test_seq) :
         # Compute storage cost
         sum_date = 0
-        for c in range(self.C):
-            cnt = 0
-            for j in range(self.J):
-                if test_seq[j] == c:
-                    cnt += 1
-                if c in self.day_demands[j]:
-                    cnt -= 1
-                sum_date += cnt
+        cnt = 0
+        for j in range(self.J):
+            c = test_seq[j]
+            if c > -1:
+                cnt += 1
+            cnt -= len(self.day_demands[j])
+            sum_date += cnt
         # Compute transition costs
         sum_tran = 0
         last_c = -1
@@ -772,28 +824,35 @@ def solve(instance: Instance) -> Solution:
     Returns:
         Solution: A solution object initialized with an iterator on Edge object
     """
-    TIME_LIMIT_SEC = get_time_limit(instance)
-    #TIME_LIMIT_SEC = 30
+    #TIME_LIMIT_SEC = get_time_limit(instance)
+    TIME_LIMIT_SEC = 60
     TIME_LIMIT_MARGIN_SEC = 15
 
     timeout_time = time.time() + TIME_LIMIT_SEC - TIME_LIMIT_MARGIN_SEC
 
-    solver = SolverAdvanced(instance, timeout_time)
-    # Set initial solution
-    init_seq = solver.solve_heuristic()
-    #init_seq = solver.solve_naive_random()
-    #init_seq = solver.naive_solution()
-    solver.set_initial_seq(init_seq)
+    best_seq = None
+    best_seq_cost = 1e10
 
-    #solver.solve_move_1()
-    #solver.solve_move_k(6)
-    #solver.solve_hill_climbing_2_opt()
-    solver.solve_VND()
+    while time.time() < timeout_time:
+        solver = SolverAdvanced(instance, timeout_time)
+        # Set initial solution
+        #init_seq = solver.solve_heuristic()
+        init_seq = solver.solve_naive_random()
+        #init_seq = solver.naive_solution()
+        solver.set_initial_seq(init_seq)
 
-    # solver.best_seq = solver.solve_heuristic()
+        #solver.solve_move_1()
+        #solver.solve_move_k(6)
+        #solver.solve_hill_climbing_2_opt()
+        solver.solve_VND()
+        # solver.best_seq = solver.solve_heuristic()
+
+        if solver.best_cost < best_seq_cost:
+            best_seq_cost = solver.best_cost
+            best_seq = solver.best_seq[:]
     
     
-    sol = Solution(solver.best_seq)
+    sol = Solution(best_seq)
 
     return sol
 
